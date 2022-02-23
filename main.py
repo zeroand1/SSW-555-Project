1a744294dcda7159819eb88b61a1a0818a8ebdf4
Samarth Kapuria <zeroand1@users.noreply.github.com>
2022-02-23 09:18:42 -0500

    individuals

--- main.py
+++ main.py
@@ -1,79 +1,98 @@
-# This is a sample Python script.
+from gedcom.element.individual import IndividualElement
+from gedcom.parser import Parser
+import gedcom
+import datetime
+from prettytable import PrettyTable
+x = PrettyTable()
+y = PrettyTable()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Path to your `.ged` file
+file_path = 'Seinfelds.ged'
 
+# Initialize the parser
+gedcom_parser = Parser()
 
-def validator(level,tag,arg):
-    validLevels = ['0', "1", '2']
-    validTagIndi = ["INDI","FAM"]
-    validTagZero = ['HEAD','TRLR',"NOTE"]
-    validTagOne = ['NAME',"SEX", "BIRT", "DEAT", "FAMC", "FAMS","MARR","HUSB","WIFE","CHIL","DIV"]
-    if level == '0':
-        if arg in validTagIndi:
-            return True
-        if tag in validTagZero:
-            return True
+# Parse your file
+gedcom_parser.parse_file(file_path)
+
+root_child_elements = gedcom_parser.get_root_child_elements()
+
+# families = gedcom_parser.get_families()
+#
+# for family in families:
+#     print(family)
+# Iterate through all root child elements
+unique_individuals = []
+for element in root_child_elements:
+    
+    # Is the `element` an actual `IndividualElement`? (Allows usage of extra functions such as `surname_match` and `get_name`.)
+    if isinstance(element, IndividualElement):
+        if element in unique_individuals:
+            pass
         else:
-            return False
-    if level == '1':
-        if tag in validTagOne:
-            return True
-        else:
-            return False
-    if level == "2":
-        if tag == "DATE":
-            return True
-        else:
-            return False
+            unique_individuals.append(element)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-
-    with open('Sample.txt', 'r') as file:
-        lines = file.readlines()
-
-    # sample = "01 2345"
-    # print(sample.strip()[sample.strip().index(' ')+1:])
-    for line in lines:
-
-        valid = False
-
-        line = line.strip()
-        lineOne = "--> " + line
-        print(lineOne)
-        lineTwo = "<-- "
-
-        try:
-            level = line[0:line.index(' ')]
-        except:
-            level = line
-        lineTwo += level + '|'
-
-        try:
-            rest1 = line[line.index(' ') + 1:]
-        except:
-            rest1 = ""
-
-        try:
-            tag = rest1[0: rest1.index(' ')]
-        except:
-            tag = rest1
-
-        try:
-            rest2 = rest1[rest1.index(' ') + 1:]
-        except:
-            rest2 = ""
-
-        if validator(level,tag,rest2):
-            if level == '0' and rest2 in ['INDI','FAM']:
-                lineTwo += rest2 + "|Y|" + tag
+        unique_family = []
+        # Print the first and last name of the found individual
+        for one in gedcom_parser.get_families(element, family_type=gedcom.tags.GEDCOM_TAG_FAMILY_SPOUSE):
+            if one in unique_family:
+                pass
             else:
-                lineTwo += tag + "|Y|" + rest2
+                unique_family.append(one)
+
+        # for fam in unique_family:
+        #     for two in gedcom_parser.get_family_members(fam):
+        #         pass
+        #         # print(two.get_name())
+
+
+x.field_names  = ['ID', 'Name','Gender','Birthdate', 'Age', 'Alive','Death']
+# headers = ['ID', 'Name','Gender','Birthdate', 'Age', 'Alive', 'Death', 'Child', 'Spouse']
+rows = []
+for element in unique_individuals:
+    temp_row = []
+    temp_row.append(str(element).split(' ')[1])
+    temp_row.append(element.get_name())
+    temp_row.append(element.get_gender())
+    temp_row.append(element.get_birth_data()[0])
+    try:
+        deathyear = None
+        birthdate = datetime.datetime.strptime(element.get_birth_data()[0], '%d %b %Y')
+        deathyear = element.get_death_year()
+        if deathyear>0:
+            age = deathyear - birthdate.year
         else:
-            lineTwo += tag + "|N|" + rest2
+            today = datetime.datetime.today()
+            age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+        # print(age)
+        temp_row.append(age)
+    except:
+        temp_row.append('N/A')
+        # print('ERROR: Birthdate not found for element' + str(element))
 
-        print(lineTwo)
+    temp_row.append(not element.is_deceased())
+    temp_row.append(element.get_death_data()[0])
+    # for tabla in gedcom_parser.get_families(element, family_type=gedcom.tags.GEDCOM_TAG_FAMILY_SPOUSE):
+    #     sd = tabla.get_child_elements()
+    #     # print(sd)
+    #     for d in sd:
+    #         print(d.to_gedcom_string())
+    #     print("-----")
+    # temp_row.append("?")
+    # temp_row.append(element.get_family_members()[1])
+    x.add_row(temp_row)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+y.field_names = ['ID', 'Married','Divorced','Husband ID', 'Husband Name', 'WideID','Wife Name']
+rows_y = []
+print(unique_family.__len__())
+for element in unique_family:
+    temp_row = []
+    print(element)
+    # for member in gedcom_parser.get_family_members(element):
+    #     print(member)
+
+print(x)
+print(y)
+
+
