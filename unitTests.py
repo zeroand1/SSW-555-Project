19f78205126b3bdc90c9243af7d438bc810eb130
aanjaria1213 <83485362+aanjaria1213@users.noreply.github.com>
2022-03-23 15:35:04 -0400

    Add files via upload

--- unitTests.py
+++ unitTests.py
@@ -48,5 +48,39 @@ class TestUserStory5(unittest.TestCase):
         individual, families = reader("birthb4marriage_1.ged")
         self.assertTrue(userStories.marriage_before_death(individual, families))  # add assertion here
 
+class TestUserStory7(unittest.TestCase):
+    def test_1(self):
+        individual, families = reader("Seinfelds.ged")#good file
+        self.assertEqual(userStories.less_than_150_years_old(individual, families), True)
+    def test_2(self):
+        individual, families = reader("Seinfelds.ged")
+        self.assertNotEqual(userStories.less_than_150_years_old(individual, families), True)
+    def test_3(self):
+        individual, families = reader("Seinfelds.ged")#for bad file
+        self.assertIsNotNone(userStories.less_than_150_years_old(individual, families))
+    def test_4(self):
+        individual, families = reader("Seinfelds.ged")#for good file
+        self.assertIsNotNone(userStories.less_than_150_years_old(individual, families))
+    def test_5(self):
+        individual, families = reader("Seinfelds.ged")
+        self.assertTrue(userStories.less_than_150_years_old(individual, families))  # add assertion here
+
+class TestUserStory10(unittest.TestCase):
+    def test_1(self):
+        individual, families = reader("Seinfelds.ged")#good file
+        self.assertEqual(userStories.marriage_after_14(individual, families), True)
+    def test_2(self):
+        individual, families = reader("Seinfelds.ged")
+        self.assertNotEqual(userStories.marriage_after_14(individual, families), True)
+    def test_3(self):
+        individual, families = reader("Seinfelds.ged")#for bad file
+        self.assertIsNotNone(userStories.marriage_after_14(individual, families))
+    def test_4(self):
+        individual, families = reader("Seinfelds.ged")#for good file
+        self.assertIsNotNone(userStories.marriage_after_14(individual, families))
+    def test_5(self):
+        individual, families = reader("Seinfelds.ged")
+        self.assertTrue(userStories.marriage_after_14(individual, families))  # add assertion here
+
 if __name__ == '__main__':
     unittest.main()
